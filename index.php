96c3dd3510e1aef21b7b52e901da1cfa1b0d57fc
New world <73671344+Cyber55Jobs@users.noreply.github.com>
2022-04-30 13:00:47 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+#customers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #263244;
+  color: white;
+}
+h2{
+	color: #696902;
+}
+.logo{
+	background-color: white;
+	width: 100%;
+}
+</style>
+</head>
+<body background="./S.png">
+<div class="logo">
+<img src="https://web.ua.es/secciones-ua/images/layout/logo-ua.jpg" width="300" ><img src="https://web.ua.es/secciones-ua/images/layout/logo-crueconucrania.jpg">
+</div>
+<h1>Este título es original avalado por el ministerio de educación europeo (eSET)</h1>
+<div class="logo">
+
+
+<h2 align="center">
+	Jairo Jeremy Castro Fortunato
+ha superado los estudios universitarios correspondientes organizados por la Facultad Informatica,
+conforme a un plan de estudios homologado por el Consejo de Universidades,
+ expide el presente titulo universitario oficial de
+Ingeniero en Sistemas informáticos
+con validez, en todo el territorio nacional, que faculta al interesado  para disfrutar
+los derechos que a este titulo otorgan las disposiciones vigentes.
+Dado en Alicante, el 03 de febrero de 2022
+</h2>
+<p align="center">La informacion mostrada debe coincidir con la informacion en el titulo eletronico</p>
+
+<table id="customers">
+  <tr>
+    <th>Nombre y apellido</th>
+    <th>Tipo de documento de identificación</th>
+    <th>País de nacimiento</th>
+
+        <th>Cédula extranjera</th>
+    <th>ID de estudiante</th>
+    <th>Tipo de estudio</th>
+  </tr>
+  <tr>
+    <td>Jairo Jeremy Castro Fortunato</td>
+    <td>Cédula Extranjera</td>
+    <td>República Dominicana</td>
+        <td>402-1548848-3</td>
+    <td>402-1548848-3 NRNT 2019BNFFGS</td>
+    <td>Campus virtual</td>
+  </tr>
+
+
+</table>
+</div>
+<iframe src="Se.html" style="height:6500px;width:100%" title="Suplemento europeo al título"></iframe>
+<div class="logo">
+	<h2><a href="./Se.pdf">Descargar Complemento</a></h2>
+</div>
+
+</body>
+
+</html>
+
+
